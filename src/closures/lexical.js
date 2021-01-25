999c2fadeebf0e2b4ccfa774fe44a7b29a266f61
Otoniel Andrade <otonielandrade7@gmail.com>
2021-01-25 11:35:37 -0600

    Ámbito léxico en closures

---
+++ src/closures/lexical.js
@@ -0,0 +1,15 @@
+const buildCount = (i) => {
+  let count = i;
+  const displayCount = () => {
+    console.log(count++);
+  };
+  return displayCount;
+};
+const myCount = buildCount(2);
+myCount();
+myCount();
+myCount();
+
+const myOtherCount= buildCount(10);
+myOtherCount();
+myOtherCount();
