0ad9054c92c4eb7490577bf5ebe60d3af9f58689
Otoniel Andrade <otonielandrade7@gmail.com>
2021-01-25 10:37:58 -0600

    Local Scope

---
+++ src/scope/local.js
@@ -0,0 +1,19 @@
+const helloWorld = () => {
+    const hello = 'Hello world';
+    console.log(hello);
+}
+helloWorld();
+console.log(hello);
+
+
+var scope = 'i am global';
+
+const functionScope = () => {
+    var scope = 'I am just a local';
+    const func = () => {
+        return scope;
+    }
+    console.log(func());//I am just a local
+}
+functionScope();
+console.log(scope);//i am global
